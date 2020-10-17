c923e89b7b92dc24262b19b63d32d4bfb88e96b6
İdil Sülo <idilsulo@gmail.com>
2020-10-17 18:26:51 +0300

    Separated playlist page, added threading to load the tracks of user while waiting on the analysis page

--- web-app/app.py
+++ web-app/app.py
@@ -13,6 +13,9 @@ from flask import render_template, request, redirect, session
 from flask_session import Session
 import time
 import uuid
+import pandas as pd
+
+import threading
 
 from analysis import *
 
@@ -22,12 +25,28 @@ app.config['SECRET_KEY'] = os.urandom(64)
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_FILE_DIR'] = './.flask_session/'
 Session(app)
+
+
 caches_folder = './.spotify_caches/'
 if not os.path.exists(caches_folder):
     os.makedirs(caches_folder)
 
+sp = None
+all_tracks = pd.DataFrame()
+t = None
+
 def session_cache_path():
     return caches_folder + session.get('uuid')
+
+def set_all_tracks(sp):
+    global all_tracks
+    # playlist = return_playlist(sp, danceability='low', instrumentalness='high')
+    # track_names, track_uris = get_playlist_tracks(sp, playlist)
+    # return render_template('playlist.html', track_uris=track_uris, track_names=track_names)
+
+    all_tracks = return_all_tracks(sp)
+    print("All tracks of the user is set.")
+    
 
 @app.route('/')
 @app.route('/index')
@@ -45,6 +64,8 @@ def callback():
 # @app.route('/my-analysis', methods=['POST', 'GET'])
 @app.route('/my-analysis')
 def my_analysis():
+    global sp
+    global t
     if not session.get('uuid'):
         # Step 1. Visitor is unknown, give random ID
         session['uuid'] = str(uuid.uuid4())
@@ -70,14 +91,29 @@ def my_analysis():
     #        * Past 3 years
 
     # Get the top genres of user based on top artists
-    sp, user_name, top_genres_data = user_top_genres(auth_manager, term='medium_term')
+    spotify, user_name, top_genres_data = user_top_genres(auth_manager, term='medium_term')
+    sp = spotify
     top_genres_data = top_genres_data.decode()
+    t = threading.Thread(target=set_all_tracks, args=(sp,))
+    t.start()
+    return render_template('analysis.html', user=user_name, top_genres=top_genres_data)
 
-    playlist = return_playlist(sp, danceability='low', instrumentalness='high')
-    track_names, track_uris = get_playlist_tracks(sp, playlist)
 
-    return render_template('analysis.html', user=user_name, top_genres=top_genres_data, 
-                                            track_uris=track_uris, track_names=track_names)
+# @app.route('/my-playlist', methods=['POST', 'GET'])
+@app.route('/my-playlist')
+def my_playlist():
+    global sp
+    global t
+    # playlist = return_playlist(sp, danceability='low', instrumentalness='high')
+    # track_names, track_uris = get_playlist_tracks(sp, playlist)
+    # return render_template('playlist.html', track_uris=track_uris, track_names=track_names)
+    if len(all_tracks) == 0:
+        print("Waiting for thread to join.")
+        t.join()
+
+    playlist = return_playlist(sp=sp, df=all_tracks, danceability='low', instrumentalness='high')
+    track_names, _ = get_playlist_tracks(sp, playlist)
+    return render_template('playlist.html', track_names=track_names)
 
 
 if __name__ == '__main__':
