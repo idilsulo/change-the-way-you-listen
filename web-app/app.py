8e493323f06fc98d5e17ffa7dcda7e36c6c98954
İdil Sülo <idilsulo@gmail.com>
2020-10-18 08:37:10 +0300

    The joy of seeing your app work end-to-end for the first time trying to concurrently update your about page

--- web-app/app.py
+++ web-app/app.py
@@ -38,6 +38,22 @@ t = None
 def session_cache_path():
     return caches_folder + session.get('uuid')
 
+def convert_features(features):
+    description = ""
+    for feature, value in features.items():
+        if int(value) < 25:
+            description += "Low {} - ".format(feature)
+        elif int(value) > 70:
+            description += "High {} - ".format(feature)
+
+    if len(description) > 5: 
+        return description[:-2]
+    else: 
+        return descrip
+
+
+
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -60,7 +76,7 @@ def my_analysis():
         # Step 1. Visitor is unknown, give random ID
         session['uuid'] = str(uuid.uuid4())
 
-    scope = 'user-read-recently-played user-top-read user-modify-playback-state'
+    scope = 'user-read-recently-played user-top-read playlist-modify-public playlist-modify-private user-library-read'
     auth_manager = spotipy.oauth2.SpotifyOAuth(scope=scope,
                                                 cache_path=session_cache_path(),
                                                 show_dialog=True)
@@ -96,7 +112,7 @@ def my_playlist():
         # Step 1. Visitor is unknown, give random ID
         session['uuid'] = str(uuid.uuid4())
 
-    scope = 'user-read-recently-played user-top-read user-modify-playback-state'
+    scope = 'user-read-recently-played user-top-read playlist-modify-public playlist-modify-private user-library-read'
     auth_manager = spotipy.oauth2.SpotifyOAuth(scope=scope,
                                                 cache_path=session_cache_path(),
                                                 show_dialog=True)
@@ -114,11 +130,19 @@ def my_playlist():
 
     sp, all_tracks, user_name = return_all_tracks(auth_manager)
     playlist = return_playlist(sp=sp, df=all_tracks)
-    track_names, _ = get_playlist_tracks(sp, playlist)
-    return render_template('playlist.html', user=user_name, track_names=track_names, features={})
+    track_names, track_ids = get_playlist_tracks(sp, playlist)
+    string = ""
+    for i in track_ids:
+        string += i
+        string += " "
+    return render_template('playlist.html', user=user_name, track_names=track_names, 
+                                            track_ids=string[:-1], features={})
 
 @app.route('/customized-playlist', methods=['POST'])
 def customized_playlist():
+
+    # TO-DO: Return playlist features e.g: high danceability, low energy 
+    #        together with playlist as a string
 
     features = {}
     features['danceability']     = request.form['danceability']
@@ -134,7 +158,7 @@ def customized_playlist():
         # Step 1. Visitor is unknown, give random ID
         session['uuid'] = str(uuid.uuid4())
 
-    scope = 'user-read-recently-played user-top-read user-modify-playback-state'
+    scope = 'user-read-recently-played user-top-read playlist-modify-public playlist-modify-private user-library-read'
     auth_manager = spotipy.oauth2.SpotifyOAuth(scope=scope,
                                                 cache_path=session_cache_path(),
                                                 show_dialog=True)
@@ -154,8 +178,59 @@ def customized_playlist():
     playlist = return_playlist(sp=sp, df=all_tracks, features=features)
     print("Printing...")
     print(playlist)
-    track_names, _ = get_playlist_tracks(sp, playlist)
-    return render_template('playlist.html', user=user_name, track_names=track_names, features=features)
+    track_names, track_ids = get_playlist_tracks(sp, playlist)
+
+    string = ""
+    for i in track_ids:
+        string += i
+        string += " "
+
+    description = convert_features(features)
+    return render_template('playlist.html', user=user_name, track_names=track_names, track_ids=string[:-1],
+                                            features=features, description=description)
+
+@app.route('/save-playlist', methods=['POST'])
+def save_playlist():
+    if not session.get('uuid'):
+        # Step 1. Visitor is unknown, give random ID
+        session['uuid'] = str(uuid.uuid4())
+
+    scope = 'user-read-recently-played user-top-read playlist-modify-public playlist-modify-private user-library-read'
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope=scope,
+                                                cache_path=session_cache_path(),
+                                                show_dialog=True)
+
+    if request.args.get("code"):
+        # Step 3. Being redirected from Spotify auth page
+        auth_manager.get_access_token(request.args.get("code"))
+        return redirect('/')
+
+    if not auth_manager.get_cached_token():
+        # Step 2. Display sign in link when no token
+        auth_url = auth_manager.get_authorize_url()
+        return f'<h2><a href="{auth_url}">Sign in</a></h2>'
+
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+    user_name = sp.current_user()['display_name']
+    user_id = sp.current_user()['id']
+    name = request.form['playlist-name']
+    tracks = request.form['tracks']
+    print(tracks)
+    # for t in tracks:
+    #     print(t)
+    sp.user_playlist_create(user=user_id, name=name, public=True)
+    p_id = None
+    for playlist in sp.user_playlists(user_id)['items']:
+        print(playlist['name'])
+        if playlist['name'] == name:
+            p_id = playlist['id']
+            break
+
+    tracks = tracks.split(" ")
+    tracks = [t[1:-1] for t in tracks]
+    print(tracks)
+    sp.user_playlist_add_tracks(user=user_id, playlist_id=p_id, tracks=tracks)
+    return render_template('save-playlist.html', user=user_name, name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
